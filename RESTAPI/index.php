8e1ad12c07c571cc4143ecfb4fa32d4a0e8df726
Michael Ntambazi <michael.ntambazi@gmail.com>
2016-05-07 12:26:17 +0300

    Separated RESTAPI logic from TodoListApp logic by creating to folders RESTAPI and TodoListApp to house the two logics respectively

--- RESTAPI/index.php
+++ RESTAPI/index.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michaelntambazi
- * Date: 07/05/16
- * Time: 10:25 AM
- */
-
-/*
- * This index.php file will act as a front controller for the API,
- *  so all requests to the API server will be made through this file.
- *
- * DUTIES
- * 1 - Accept an API call with any number of parameters
- * 2 - Extract the Controller and Action for the API call
- * 3 - Make the necessary checks to ensure that the Controller and Action exist
- * 4 - Execute the API call
- * 5 - Catch errors, if any
- * 6 - Send back a result to the caller
- */
-
 // Define path to data folder
 define('DATA_PATH', realpath(dirname(__FILE__).'/data'));
 
@@ -26,7 +6,6 @@ define('DATA_PATH', realpath(dirname(__FILE__).'/data'));
 $applications = array(
     'APP001' => '28e336ac6c9423d946ba02d19c6a2632', //randomly generated app key
 );
-
 //include our models
 include_once 'models/TodoItem.php';
 
@@ -83,7 +62,7 @@ try {
     $result['data'] = $controller->$action();
     $result['success'] = true;
 
-} catch (Exception $e) {
+} catch( Exception $e ) {
     //catch any exceptions and report the problem
     $result = array();
     $result['success'] = false;
